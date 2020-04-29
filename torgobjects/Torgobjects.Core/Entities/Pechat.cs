e50f28fa83ac6e603f1af6886023362d7684f9ee
FedorovaNK <64545483+FedorovaNK@users.noreply.github.com>
2020-04-29 23:05:57 +0300

    Add files via upload

---
+++ torgobjects/Torgobjects.Core/Entities/Pechat.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Torgobjects.Core.Entities
+{
+    public class pechat
+    {
+        public long Id { get; set; }
+
+        public string ContractNumber { get; set; }
+
+        public string Name { get; set; }
+
+        public string District { get; set; }
+
+        public string FacilityArea { get; set; }
+
+        public string AdmArea { get; set; }
+
+        public string Address { get; set; }
+
+        public string PeriodOfPlacement { get; set; }
+
+        public string NameOfBusinessEntity { get; set; }
+
+    }
+}
